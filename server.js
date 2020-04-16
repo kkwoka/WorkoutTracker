a179d1b5288b1611e3a680d9dc90154c33866bb6
Kendra Kwoka <kendrakwoka@gmail.com>
2020-04-16 14:18:00 -0500

    removing commented out lines

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 const express = require("express");
-// const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
 
 
 const PORT = process.env.PORT || 3000;
 
-// const User = require("./userModel.js");
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  // useCreateIndex: true
 });
-
-// app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -27,4 +22,4 @@ require("./Develop/routes/html-routes")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
-  });  
+});  
